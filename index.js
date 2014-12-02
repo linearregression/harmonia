81dd8c682cbebaaf3736be40f7766aa5608838e3
Ben Youngblood <ben.youngblood@colonyamerican.com>
2014-12-02 09:23:31 -0700

    simple factory method to create amqp connection for sharing

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var Coyote = require('coyote');
+
 exports.Server = require('./lib/Server');
 exports.Client = require('./lib/Client');
 exports.ErrorResponse = require('./lib/client/ErrorResponse');
+
+exports.createAmqpConnection = function(amqpUrl) {
+  return Coyote.createConnection(amqpUrl)
+    .then(function(connection) {
+      return connection;
+    });
+};
