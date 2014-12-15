5c33b0235abb5dba084c3f92fc983f63191a76c4
Ben Youngblood <ben.youngblood@colonyamerican.com>
2014-12-15 11:38:02 -0700

    export response class

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Coyote = require('coyote');
 
 exports.Server = require('./lib/Server');
 exports.Client = require('./lib/Client');
+exports.Response = require('./lib/Response');
 exports.ErrorResponse = require('./lib/client/ErrorResponse');
 
 exports.createAmqpConnection = function(amqpUrl, socketOptions) {
